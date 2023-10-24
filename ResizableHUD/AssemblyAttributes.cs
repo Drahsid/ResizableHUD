cec39d4644df075c5ae4e6929272f8ed53f34352
Drahsid <Drahsid@gmail.com>
2023-10-24 10:23:15 -0400

    Bump version

--- ResizableHUD/AssemblyAttributes.cs
+++ ResizableHUD/AssemblyAttributes.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 [assembly: System.Reflection.AssemblyProduct("ResizableHUD")]
 [assembly: System.Reflection.AssemblyTitle("ResizableHUD")]
 
-[assembly: System.Reflection.AssemblyVersion("1.2.0")]
+[assembly: System.Reflection.AssemblyVersion("1.2.1")]
 
 #if DEBUG
 [assembly: System.Reflection.AssemblyConfiguration("Debug")]
